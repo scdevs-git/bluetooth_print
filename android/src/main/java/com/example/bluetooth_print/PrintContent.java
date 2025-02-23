bf213398c0448be4501cb9920e362b62d1754d7c
miljan-anicic <anicic.miljan@scdevs.ba>
2025-02-23 04:14:35 +0100

    Update PrintContent.java

--- android/src/main/java/com/example/bluetooth_print/PrintContent.java
+++ android/src/main/java/com/example/bluetooth_print/PrintContent.java
@@ -21,100 +21,65 @@ public class PrintContent {
       /**
        * 票据打印对象转换
        */
-      public static Vector<Byte> mapToReceipt(Map<String,Object> config, List<Map<String,Object>> list) {
-            EscCommand esc = new EscCommand();
-            //初始化打印机
-            esc.addInitializePrinter();
-            //打印走纸多少个单位
-            esc.addPrintAndFeedLines((byte) 1);
+ public static Vector<Byte> mapToReceipt(Map<String,Object> config, List<Map<String,Object>> list) {
+    EscCommand esc = new EscCommand();
+    esc.addInitializePrinter();
+    esc.addPrintAndFeedLines((byte) 1);
 
-            // {type:'text|barcode|qrcode|image', content:'', size:4, align: 0|1|2, weight: 0|1, width:0|1, height:0|1, underline:0|1, linefeed: 0|1}
-            for (Map<String,Object> m: list) {
-                  String type = (String)m.get("type");
-                  String content = (String)m.get("content");
-                  int align = (int)(m.get("align")==null?0:m.get("align"));
-                  int size = (int)(m.get("size")==null?3:m.get("size"));
-                  int weight = (int)(m.get("weight")==null?0:m.get("weight"));
-                  int width = (int)(m.get("width")==null?0:m.get("width"));
-                  int height = (int)(m.get("height")==null?0:m.get("height"));
-                  int underline = (int)(m.get("underline")==null?0:m.get("underline"));
-                  int linefeed = (int)(m.get("linefeed")==null?0:m.get("linefeed"));
+    for (Map<String,Object> m : list) {
+        String type = (String) m.get("type");
+        String content = (String) m.get("content");
+        int align = (int)(m.get("align") == null ? 0 : m.get("align"));
+        int size = (int)(m.get("size") == null ? 3 : m.get("size"));
+        int weight = (int)(m.get("weight") == null ? 0 : m.get("weight"));
+        int width = (int)(m.get("width") == null ? 0 : m.get("width"));
+        int height = (int)(m.get("height") == null ? 0 : m.get("height"));
+        int linefeed = (int)(m.get("linefeed") == null ? 0 : m.get("linefeed"));
 
-                  EscCommand.ENABLE emphasized = weight==0?EscCommand.ENABLE.OFF:EscCommand.ENABLE.ON;
-                  EscCommand.ENABLE doublewidth = width==0?EscCommand.ENABLE.OFF:EscCommand.ENABLE.ON;
-                  EscCommand.ENABLE doubleheight = height==0?EscCommand.ENABLE.OFF:EscCommand.ENABLE.ON;
-                  EscCommand.ENABLE isUnderline = underline==0?EscCommand.ENABLE.OFF:EscCommand.ENABLE.ON;
+        Log.d("PrintDebug", "Type: " + type + ", Content: " + content + ", Width: " + width + ", Height: " + height);
 
-                  // 设置打印位置
-                  esc.addSelectJustification(align==0?EscCommand.JUSTIFICATION.LEFT:(align==1?EscCommand.JUSTIFICATION.CENTER:EscCommand.JUSTIFICATION.RIGHT));
+        esc.addSelectJustification(align == 0 ? EscCommand.JUSTIFICATION.LEFT : 
+                                   (align == 1 ? EscCommand.JUSTIFICATION.CENTER : EscCommand.JUSTIFICATION.RIGHT));
 
-                  if("text".equals(type)){
-                        int absolutePos = (int)(m.get("absolutePos")==null?0:m.get("absolutePos"));
-                        int relativePos = (int)(m.get("relativePos")==null?0:m.get("relativePos"));
-                        int fontZoom = (int)(m.get("fontZoom")==null?1:m.get("fontZoom"));
-                        short aPos = (short)absolutePos;
-                        short rPos = (short)relativePos;
-                        Log.e(TAG,"******************* absolutePos: " + aPos +", relativePos: " + rPos +", fontZoom: " + fontZoom);
+        if ("text".equals(type)) {
+            esc.addSelectPrintModes(EscCommand.FONT.FONTA, weight > 0 ? EscCommand.ENABLE.ON : EscCommand.ENABLE.OFF,
+                                    height > 0 ? EscCommand.ENABLE.ON : EscCommand.ENABLE.OFF,
+                                    width > 0 ? EscCommand.ENABLE.ON : EscCommand.ENABLE.OFF, EscCommand.ENABLE.OFF);
+            esc.addText(content);
+            esc.addSelectPrintModes(EscCommand.FONT.FONTA, EscCommand.ENABLE.OFF, EscCommand.ENABLE.OFF, EscCommand.ENABLE.OFF, EscCommand.ENABLE.OFF);
+        } else if ("barcode".equals(type)) {
+            esc.addSelectPrintingPositionForHRICharacters(EscCommand.HRI_POSITION.BELOW);
+            esc.addSetBarcodeHeight((byte) 60);
+            esc.addSetBarcodeWidth((byte) 2);
+            esc.addCODE128(esc.genCodeB(content));
+        } else if ("qrcode".equals(type)) {
+            esc.addSelectErrorCorrectionLevelForQRCode((byte) 0x31);
+            esc.addSelectSizeOfModuleForQRCode((byte) size);
+            esc.addStoreQRCodeData(content);
+            esc.addPrintQRCode();
+        } else if ("image".equals(type)) {
+            byte[] bytes = Base64.decode(content, Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
 
-                        // 设置绝对打印位置，将当前打印位置设置到距离行首 n* hor_motion_unit 点
-                        esc.addSetAbsolutePrintPosition(aPos);
-                        // 设置相对打印位置，将打印位置设置到距当前位置 n 点处
-                        esc.addSetRelativePrintPositon(rPos);
-                        // 设置为倍高倍宽
-                        esc.addSelectPrintModes(EscCommand.FONT.FONTA, emphasized, doubleheight, doublewidth, isUnderline);
-                        if(fontZoom>1){
-                              esc.addSetKanjiFontMode(EscCommand.ENABLE.ON, EscCommand.ENABLE.ON, EscCommand.ENABLE.OFF);
-                        }else{
-                              esc.addSetKanjiFontMode(EscCommand.ENABLE.OFF, EscCommand.ENABLE.OFF, EscCommand.ENABLE.OFF);
-                        }
-                        esc.addText(content);
-                        // 取消倍高倍宽
-                        esc.addSelectPrintModes(EscCommand.FONT.FONTA, EscCommand.ENABLE.OFF, EscCommand.ENABLE.OFF, EscCommand.ENABLE.OFF, EscCommand.ENABLE.OFF);
-                  }else if("barcode".equals(type)){
-                        esc.addSelectPrintingPositionForHRICharacters(EscCommand.HRI_POSITION.BELOW);
-                        // 设置条码可识别字符位置在条码下方
-                        // 设置条码高度为60点
-                        esc.addSetBarcodeHeight((byte) 60);
-                        // 设置条码宽窄比为2
-                        esc.addSetBarcodeWidth((byte) 2);
-                        // 打印Code128码
-                        esc.addCODE128(esc.genCodeB(content));
-                  }else if("qrcode".equals(type)){
-                        // 设置纠错等级
-                        esc.addSelectErrorCorrectionLevelForQRCode((byte) 0x31);
-                        // 设置qrcode模块大小
-                        esc.addSelectSizeOfModuleForQRCode((byte) size);
-                        // 设置qrcode内容
-                        esc.addStoreQRCodeData(content);
-                        // 打印QRCode
-                        esc.addPrintQRCode();
-                  }else if("image".equals(type)){
-                        byte[] bytes = Base64.decode(content, Base64.DEFAULT);
-                        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-                        esc.addRastBitImage(bitmap, 576, 0);
-                  }
+            int imgWidth = width > 0 ? width : bitmap.getWidth();
+            int imgHeight = height > 0 ? height : bitmap.getHeight();
 
-                  if(linefeed == 1){
-                        //打印并换行
-                        esc.addPrintAndLineFeed();
-                  }
+            Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap, imgWidth, imgHeight, false);
+            Log.d("PrintDebug", "Resized Image Width: " + imgWidth + ", Height: " + imgHeight);
 
-            }
+            esc.addRastBitImage(resizedBitmap, imgWidth, 0);
+        }
 
-            //打印走纸n个单位
-            esc.addPrintAndFeedLines((byte) 1);
+        if (linefeed == 1) {
+            esc.addPrintAndLineFeed();
+        }
+    }
 
-            // 开钱箱
-            esc.addGeneratePlus(LabelCommand.FOOT.F2, (byte) 255, (byte) 255);
-            //开启切刀
-            esc.addCutPaper();
-            //添加缓冲区打印完成查询
-            byte [] bytes={0x1D,0x72,0x01};
-            //添加用户指令
-            esc.addUserCommand(bytes);
+    esc.addPrintAndFeedLines((byte) 1);
+    esc.addCutPaper();
+    return esc.getCommand();
+}
 
-            return esc.getCommand();
-      }
 
       /**
        * 标签打印对象转换
